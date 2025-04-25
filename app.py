aee0b1979df74b7d8fb1c3ee41206bb7e1f3a552
XaviTandazo <xavier00396@gmail.com>
2025-04-25 14:39:47 -0500

    Agregado el código de control de acceso por MAC

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ import paramiko
 
 app = Flask(__name__)
 
-# Función para conectarse al router
+# Función para conectarse al router con configuraciones compatibles
 def connect_router():
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('192.168.1.1', username='admin', password='cisco')
+
+    transport = paramiko.Transport(('192.168.1.1', 22))
+    transport.get_security_options().ciphers = ['aes128-cbc']
+    transport.get_security_options().kex = ['diffie-hellman-group14-sha1']
+    transport.get_security_options().host_key_algorithms = ['ssh-rsa']
+
+    transport.connect(username='admin', password='cisco')
+    ssh._transport = transport
     return ssh
 
 # Página principal
@@ -15,23 +22,41 @@ def connect_router():
 def index():
     return render_template('index.html')
 
-# Agregar un dispositivo autorizado
+# Agregar dispositivo autorizado (permitir MAC)
 @app.route('/add_device', methods=['POST'])
 def add_device():
     mac_address = request.form['mac_address']
-    # Conexión SSH al router y actualización de la lista MAC
+    interface = "FastEthernet1/1"  # Puedes hacerlo dinámico si quieres
+
     ssh = connect_router()
-    ssh.exec_command(f'command_to_add_mac {mac_address}')
+    commands = f'''
+    conf t
+    interface {interface}
+    switchport port-security
+    switchport port-security mac-address {mac_address}
+    end
+    write memory
+    '''
+    ssh.exec_command(commands)
     ssh.close()
     return redirect(url_for('index'))
 
-# Bloquear un dispositivo
+# Bloquear dispositivo (ponerlo como no permitido)
 @app.route('/block_device', methods=['POST'])
 def block_device():
     mac_address = request.form['mac_address']
-    # Conexión SSH al router y actualización de las reglas de filtrado
+    interface = "FastEthernet1/1"
+
     ssh = connect_router()
-    ssh.exec_command(f'command_to_block_mac {mac_address}')
+    commands = f'''
+    conf t
+    interface {interface}
+    switchport port-security mac-address {mac_address}
+    switchport port-security violation shutdown
+    end
+    write memory
+    '''
+    ssh.exec_command(commands)
     ssh.close()
     return redirect(url_for('index'))
 
